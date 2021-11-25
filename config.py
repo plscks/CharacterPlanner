3a2eb219036b2879e5dbc0b62829422510fd7307
plscks <plscks@icloud.com>
2021-11-25 14:59:21 -0600

    Changes logging and removes old sessions

--- config.py
+++ config.py
@@ -9,11 +9,12 @@ load_dotenv(path.join(basedir, '.cfg'))
 class Config(object):
     DEBUG = True
     DEVELOPMENT = True
-    FLASK_SECRET = environ.get('SECRET_KEY')
+    FLASK_SECRET = environ.get('SECRET_KEY').encode('utf8')
     SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_TYPE = environ.get('SESSION_TYPE')
+    SESSION_PERMANENT = environ.get('SESSION_PERMANENT')
     SESSION_REDIS = redis.from_url(environ.get('SESSION_REDIS'))
 
 class ProductionConfig(Config):
